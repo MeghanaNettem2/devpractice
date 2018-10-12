02ec2829466b434a3a5e70ec85e4c0db95c3101b
MeghanaNettem2 <43988345+MeghanaNettem2@users.noreply.github.com>
2018-10-12 10:24:36 +0530

    Update formController.java

--- src/main/java/com/example/form/controller/formController.java
+++ src/main/java/com/example/form/controller/formController.java
@@ -23,5 +23,10 @@ public class formController {
 	public Form create(@RequestBody Form obj) {
 		return repository.save(obj);
 	}
+// 	@PostMapping("/form")
+// 	public Form create(@RequestBody Form obj) {
+// 		return repository.save(obj);
+// 	}
+
 
 }
